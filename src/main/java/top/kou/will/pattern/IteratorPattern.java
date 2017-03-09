80101df3a36fc19695ea0f1801b0b540bf4df752
xianliangzhang <2497095564@qq.com>
2017-03-09 19:42:18 +0800

    设计模式

--- src/main/java/top/kou/will/pattern/IteratorPattern.java
+++ src/main/java/top/kou/will/pattern/IteratorPattern.java
@@ -3,10 +3,7 @@ package top.kou.will.pattern;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Administrator on 2017/3/9.
@@ -21,32 +18,80 @@ public class IteratorPattern {
         boolean hasNext();
     }
 
-    static class Container<E> implements Iterable<E> {
+    static class Container<E> {
+        private E[] container = (E[]) new Object[0];
+        private int size = 0;
 
-
-
-        @Override
-        public E first() {
-            return null;
+        public void add(E e) {
+            size ++;
+            E[] tempContainer = (E[]) new Object[size];
+            System.arraycopy(container, 0, tempContainer, 0, container.length);
+            tempContainer[size-1] = e;
+            container = tempContainer;
         }
 
-        @Override
-        public E next() {
-            return null;
+        public void remove(E e) {
+            int index = -1;
+            for (int i = 0; i < container.length; i ++) {
+                if (container[i].equals(e)) {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (-1 != index) {
+                E[] tempContainer = (E[]) new Object[container.length - 1];
+                for (int i = 0; i < container.length; i ++) {
+                    if (i < index) {
+                        tempContainer[i] = container[i];
+                    }
+                    if (i > index) {
+                        tempContainer[i - 1] = container[i];
+                    }
+                }
+                container = tempContainer;
+            }
         }
 
-        @Override
-        public boolean hasNext() {
-            return false;
+        Iterable<E> iterator() {
+            return new Iterable<E>() {
+                int i = 0;
+
+                @Override
+                public E first() {
+                    return size == 0 ? null : container[0];
+                }
+
+                @Override
+                public E next() {
+                    return container[i++];
+                }
+
+                @Override
+                public boolean hasNext() {
+                    return size == 0 ? false : i > size-1 ? false : true;
+                }
+            };
         }
     }
 
-    static <T> T go(T x, T y) {
-        return x;
-    }
 
     public static void main(String[] args) throws Exception {
-        System.out.println(go("", 2.3).getClass());
+        Container<Integer> container = new Container<Integer>();
+        container.add(3);
+        container.add(5);
+        container.add(7);
+
+        Iterable<Integer> iterable = container.iterator();
+        while (iterable.hasNext()) {
+            System.out.println(iterable.next());
+        }
+
+        container.remove(5);
+        iterable = container.iterator();
+        while (iterable.hasNext()) {
+            System.out.println(iterable.next());
+        }
     }
 
 }
